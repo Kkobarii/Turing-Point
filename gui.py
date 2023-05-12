f021caddae972bc79f3e71a9cbdd1562f7f7c35c
Barbora Kovalská <barca.kovalska@gmail.com>
2023-05-12 14:16:50 +0200

    GUI improvements, refactor

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ import io
 import numpy as np
 
 from threading import Thread
-from turing_machine import Tape, BLANK, tm_to_diagraph, load_tm
+from turing_machine import Tape, tm_to_diagraph, load_tm, BLANK
 
 
 class GUI:
@@ -13,9 +13,9 @@ class GUI:
         self.size = (1280, 720)
         self.tm = None
         self.paused = True
-        self.kill_thread = True
+        self.kill_run_thread = True
         self.run_thread = None
-        self.image_size = 1
+        self.image_size = 1     # doesn't work how I wanted it to
         self.buffer = np.zeros((self.size[1]*self.image_size, self.size[0]*self.image_size, 3), dtype=np.float32)
 
     def create_image(self) -> np.ndarray:
@@ -41,7 +41,7 @@ class GUI:
                 symbol = '_'
             with dpg.table_row(parent="tape table"):
                 if i == tape.head:
-                    dpg.add_text(symbol, color=[100, 255, 0])
+                    dpg.add_text(symbol, color=(255, 255, 0))
                 else:
                     dpg.add_text(symbol)
 
@@ -51,10 +51,12 @@ class GUI:
             while len(str(tape)) > 0 and str(tape)[-1] not in self.tm.input_alphabet:
                 tape = tape[:-1]
 
+            # wish this worked
             # dpg.configure_item("input tape", callback=lambda: None)
             # dpg.set_value("input tape", tape)
             # dpg.configure_item("input tape", callback=self.update_input)
 
+            # instead I have to do this
             dpg.delete_item("input tape")
             dpg.add_input_text(label="Input Tape", tag="input tape", callback=self.update_input, default_value=tape, parent="Input", width=-1)
             # dpg.focus_item("input tape")
@@ -67,19 +69,36 @@ class GUI:
         self.buffer[:] = 1.0
         self.buffer[:img_array.shape[0], :img_array.shape[1], :] = img_array
 
+    def update_status(self, status="working"):
+        if self.tm is not None:
+            if self.tm.is_accepted():
+                status = "accepted"
+            elif self.tm.is_rejected():
+                status = "rejected"
+            elif self.tm.is_final():
+                status = "final"
+        dpg.set_value("status", f"Status: {status}")
+
+    def step_tm(self):
+        if self.tm.is_final():
+            return
+        dpg.configure_item("input tape", enabled=False)
+        self.tm.step()
+        self.update_status()
+        self.update_graph()
+        self.update_tape_table(self.tm.tape)
+
     def run_tm_thread(self):
         while not self.tm.is_final() and not self.paused:
-            if self.kill_thread:
+            if self.kill_run_thread:
                 return
-            self.tm.step()
-            self.update_graph()
-            self.update_tape_table(self.tm.tape)
+            self.step_tm()
             time.sleep(dpg.get_value("sleep timer"))
 
     def run_tm(self):
         if not self.paused:
             self.paused = True
-            self.kill_thread = True
+            self.kill_run_thread = True
             self.run_thread.join()
             dpg.configure_item("step self.tm", enabled=True)
             dpg.configure_item("run self.tm", label="Run")
@@ -90,29 +109,19 @@ class GUI:
         dpg.configure_item("run self.tm", label="Pause")
         self.update_tape_table(self.tm.tape)
 
-        self.kill_thread = False
+        self.kill_run_thread = False
         self.paused = False
         self.run_thread = Thread(target=self.run_tm_thread)
         self.run_thread.start()
 
-    def step_tm(self):
-        if self.tm.is_final():
-            return
-        dpg.configure_item("input tape", enabled=False)
-        self.tm.step()
-        self.update_graph()
-        self.update_tape_table(self.tm.tape)
-
     def reset_tm(self):
-        dpg.configure_item("input tape", enabled=True)
-        dpg.configure_item("step self.tm", enabled=True)
         dpg.configure_item("run self.tm", label="Run")
         self.paused = True
-        self.kill_thread = True
+        self.kill_run_thread = True
         self.tm.reset()
         self.tm.tape = Tape(list(dpg.get_value("input tape")), 0)
-        self.update_tape_table(self.tm.tape)
-        self.update_graph()
+        self.update_status("initial")
+        self.unlock_input()
 
     def lock_input(self):
         dpg.configure_item("input tape", enabled=False)
@@ -136,12 +145,17 @@ class GUI:
         self.update_graph()
 
     def load_tm(self):
+        if not self.paused:
+            return
         try:
             self.tm = load_tm(dpg.get_value("File Path"))
+            self.tm.tape = Tape(list(dpg.get_value("input tape")), 0)
             self.unlock_input()
+            self.update_status("initial")
         except:
             print("Error loading TM")
             self.tm = None
+            self.update_status("null")
             self.lock_input()
 
     def run(self):
@@ -155,22 +169,27 @@ class GUI:
         with dpg.window(label="Turing Machine", tag="Turing Machine"):
             with dpg.group(horizontal=True):
                 with dpg.group(horizontal=False, width=300):
-                    with dpg.child_window(label="Input File", tag="Input File", height=80):
-                        dpg.add_input_text(label="File Path", tag="File Path", width=-1, default_value="assets/tm_two_words.json")
-                        dpg.add_button(label="Load", callback=self.load_tm, tag="load self.tm")
-
                     with dpg.child_window(label="Config", tag="Config"):
-                        with dpg.child_window(label="Input", tag="Input", height=40):
+                        with dpg.child_window(label="Input File", tag="Input File", height=85):
+                            dpg.add_text(f"Path to input file", tag="path label", color=(0, 200, 255))
+                            dpg.add_input_text(label="File Path", tag="File Path", width=-1, default_value="assets/tm_two_words.json")
+                            dpg.add_button(label="Load", callback=self.load_tm, tag="load self.tm")
+
+                        with dpg.child_window(label="Input", tag="Input", height=60):
+                            dpg.add_text(f"Input tape", tag="input label", color=(0, 200, 255))
                             dpg.add_input_text(label="Input Tape", tag="input tape", callback=self.update_input, width=-1, show=True, enabled=False)
 
-                        with dpg.child_window(label="Graph Settings", height=120, tag="Graph Settings"):
+                        with dpg.child_window(label="Graph Settings", height=130, tag="Graph Settings"):
+                            dpg.add_text(f"Controls", tag="settings label", color=(0, 200, 255))
                             dpg.add_button(label="Run", callback=self.run_tm, tag="run self.tm", enabled=False)
                             dpg.add_button(label="Step", callback=self.step_tm, tag="step self.tm", enabled=False)
                             dpg.add_button(label="Reset", callback=self.reset_tm, tag="reset self.tm", enabled=False)
                             dpg.add_slider_float(label="Sleep Timer", tag="sleep timer", min_value=0, max_value=2, default_value=1)
 
-                        with dpg.child_window(label="self.tm Info",  height=100, tag="self.tm Info"):
+                        with dpg.child_window(label="self.tm Info",  height=130, tag="self.tm Info"):
+                            dpg.add_text(f"Turing machine information", tag="info label", color=(0, 200, 255))
                             dpg.add_text(f"Alphabet: null", tag="alphabet", wrap=260)
+                            dpg.add_text(f"Status: null", tag="status", wrap=260)
                             dpg.add_text(f"Description: null", tag="description", wrap=260)
 
                         with dpg.table(header_row=True, borders_innerH=True, borders_outerH=True, borders_innerV=True, borders_outerV=True, tag="tape table"):
